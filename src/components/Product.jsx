b3ca1b8df0bac8896aef1b14d3069513813d7dfe
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-07-30 14:44:45 -0700

    add some styles for the product items

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+  cursor: pointer;
+`;
+
 const Container = styled.div`
   flex: 1;
-  margin: 5px; 
+  margin: 5px;
   min-width: 280px;
   height: 350px;
   display: flex;
@@ -14,7 +30,11 @@ const Container = styled.div`
   align-items: center;
   background-color: #f5fbfd;
   position: relative;
-`
+
+  &:hover ${Info} {
+    opacity: 1;
+  }
+`;
 
 const Circle = styled.div`
   width: 200px;
@@ -22,26 +42,12 @@ const Circle = styled.div`
   border-radius: 50%;
   background-color: white;
   position: absolute;
-`
+`;
 
 const Image = styled.img`
-height: 75%;
-z-index: 2;
-`
-
-const Info = styled.div`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-color: gray;
-  z-index: 3;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 10px;
-`
+  height: 75%;
+  z-index: 2;
+`;
 
 const Icon = styled.div`
   width: 40px;
@@ -51,11 +57,16 @@ const Icon = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-`
+  margin: 10px;
+  transition: all 0.5s ease;
 
+  &:hover {
+    background-color: #e9f5f5;
+    transform: scale(1.2);
+  }
+`;
 
-
-const Product = ({item}) => {
+const Product = ({ item }) => {
   return (
     <Container>
       <Circle />
@@ -73,6 +84,6 @@ const Product = ({item}) => {
       </Info>
     </Container>
   );
-}
+};
 
-export default Product
+export default Product;
