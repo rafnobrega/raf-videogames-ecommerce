326f23f14e941dd0663cc862505103f344522d48
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-07-30 00:50:10 -0700

    create and style slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,7 +2,7 @@ import ArrowLeftRoundedIcon from "@mui/icons-material/ArrowLeftRounded";
 import ArrowRightRoundedIcon from "@mui/icons-material/ArrowRightRounded";
 import { useState } from "react";
 import styled from "styled-components";
-// import { sliderItems } from "../data";
+import { sliderItems } from "../data";
 // import { mobile } from "../responsive";
 
 const Container = styled.div`
@@ -11,7 +11,7 @@ const Container = styled.div`
   display: flex;
   position: relative;
   overflow: hidden;
-  background-color: yellow;
+  /* background-color: #3bb3ff; */
 `;
 
 const Arrow = styled.div`
@@ -33,17 +33,81 @@ const Arrow = styled.div`
   z-index: 2;
 `;
 
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transition: all 1.0s ease;
+  transform: translateX(${props=>props.slideIndex * -100}vw);
+`;
 
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: ${(props) => props.bg};
+`;
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+  margin-top: 30px;
+  /* width: auto; */
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Description = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState();
+
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
 
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={() => handleClick("left")}>
         <ArrowLeftRoundedIcon />
       </Arrow>
-
-      <Arrow direction="right">
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide bg={item.bg}>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Description>{item.desc}</Description>
+              <Button>BUY NOW</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
         <ArrowRightRoundedIcon />
       </Arrow>
     </Container>
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://gmedia.playstation.com/is/image/SIEPDC/god-of-war-hub-hero-banner-featured-01-en-09jul21?$1600px--t$",
+    title: "PRE-ORDER & SAVE",
+    desc: "PRE-ORDER THE NEW GOW AND GET 10% OFF!",
+    bg: "#3bb3ff",
+  },
+  {
+    id: 2,
+    img: "https://arcticwolf.com/wp-content/uploads/2022/05/RB18-Max-Front_dark-gradient.png",
+    title: "MOST POPULAR",
+    desc: "LIGHTS OUT AND AWAY WE GO!",
+    bg: "#ff8e8e",
+  },
+  {
+    id: 3,
+    img: "https://www.pngmart.com/files/12/Xbox-Remote-Controller-Transparent-PNG.png",
+    title: "ACCESSORIES",
+    desc: "SCORE WITH NEW ACCESSORIES!",
+    bg: "#79ff96",
+  },
+];
