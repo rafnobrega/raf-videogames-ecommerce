c6b1223c5616bcd8ac9e3a46b809c9f24d4b01d4
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-08-02 14:32:10 -0700

    improve cart page summary

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,6 +3,7 @@ import { Announcement } from "../components/Announcement";
 import Navbar from "../components/Navbar";
 import { Footer } from "../components/Footer";
 import styled from "styled-components";
+import { Add, Remove } from "@mui/icons-material";
 
 const Container = styled.div``;
 
@@ -49,6 +50,61 @@ const Info = styled.div`
   flex: 3;
 `;
 
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
+
+const Image = styled.img`
+  width: 200px;
+`;
+
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const ProductName = styled.span``;
+
+const ProductId = styled.span``;
+
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+`;
+
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 200;
+`;
+
+const Hr = styled.hr`
+  background-color: #eee;
+  border: none;
+  height: 1px;
+`;
+
 const Summary = styled.div`
   flex: 1;
 `;
@@ -71,10 +127,71 @@ const Cart = () => {
         <Bottom>
           <Info>
             <Product>
-              
+              <ProductDetail>
+                <Image src="https://cdn.shopify.com/s/files/1/0490/7312/7591/products/gaming-shop-946837_500x.jpg?v=1620997720" />
+                <Details>
+                  <ProductName>
+                    <b>Product: </b>Playstation 5 Console Bundle
+                  </ProductName>
+                  <ProductId>
+                    <b>ID: </b>0000000000
+                  </ProductId>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>1</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 299.99</ProductPrice>
+              </PriceDetail>
+            </Product>
+
+            <Hr />
+
+            <Product>
+              <ProductDetail>
+                <Image src="https://m.media-amazon.com/images/I/61JGKhqxHxL._AC_SX522_.jpg" />
+                <Details>
+                  <ProductName>
+                    <b>Product: </b>Xbox Console with Controller
+                  </ProductName>
+                  <ProductId>
+                    <b>ID: </b>0000055555
+                  </ProductId>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>1</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 250.99</ProductPrice>
+              </PriceDetail>
             </Product>
           </Info>
-          <Summary>Summary</Summary>
+          <Summary>
+            <SummaryTitle>Order Summary</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal:</SummaryItemText>
+              <SummaryItemText>$ 00.00</SummaryItemText>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping:</SummaryItemText>
+              <SummaryItemText>$ 20.00</SummaryItemText>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount:</SummaryItemText>
+              <SummaryItemText>$ -20.00</SummaryItemText>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText type="total">Total:</SummaryItemText>
+              <SummaryItemText>$ 00.00</SummaryItemText>
+            </SummaryItem>
+            <Button>Checkout now</Button>
+          </Summary>
         </Bottom>
       </Wrapper>
       <Footer />
