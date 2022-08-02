40ae219164709bf2fa01c1792d7048694875d87d
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-08-01 18:04:33 -0700

    create buttons for the single product page

---
+++ src/pages/Product.jsx
@@ -0,0 +1,130 @@
+import { Add, Remove } from "@mui/icons-material";
+import React from "react";
+import styled from "styled-components";
+import { Announcement } from "../components/Announcement";
+import { Footer } from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 80vh;
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+
+`;
+
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+
+`;
+
+const FilterColor = styled.div`
+width: 20px;
+height: 20px;
+border-radius: 50%;
+background-color: ${props => props.color};
+margin: 0px 5px;
+cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+margin-left: 10px;
+padding: 5px;
+`;
+
+const FilterSizeOption = styled.option``;
+
+const Product = () => {
+  return (
+    <Container>
+      <Announcement />
+      <Navbar />
+
+      <Wrapper>
+        <ImgContainer>
+          <Image src="https://static-ca.gamestop.ca/images/products/770323/3max.jpg" />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>This is the title</Title>
+          <Description>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio
+            similique excepturi magni quae consectetur tempora aliquam eos velit
+            quasi alias
+          </Description>
+          <Price>$ 21.99</Price>
+          {/* <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer> */}
+          <AddContainer>
+            <AmountContainer>
+              <Remove/>
+              <Amount>1</Amount>
+              <Add/>
+            </AmountContainer>
+            <Button>Add to Cart</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default Product;
