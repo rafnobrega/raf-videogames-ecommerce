34f03972220b68428448400d3ff601a96ef51e4e
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-08-01 17:21:31 -0700

    add social media links and address

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,47 +1,77 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
-
+import MapIcon from "@mui/icons-material/Map";
+import PermPhoneMsgIcon from "@mui/icons-material/PermPhoneMsg";
+import EmailIcon from "@mui/icons-material/Email";
 
 const Container = styled.div`
-display: flex;
-
-`
+  display: flex;
+`;
 const Left = styled.div`
-flex: 1;
-display: flex;
-flex-direction: column;
-padding: 20px;
-`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
 
-const Logo = styled.h1`
-
-`
+const Logo = styled.h1``;
 const Description = styled.p`
-margin: 20px 0px;
-`
+  margin: 20px 0px;
+`;
 const SocialContainer = styled.div`
-display: flex;
-`
+  display: flex;
+`;
 const SocialIcon = styled.div`
-width: 40px;
-height: 40px;
-border-radius: 50%;
-color: white;
-background-color: #${props=>props.color};
-align-items: center;
-justify-content: center;
-margin-right: 20px;
-`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
 
 const Center = styled.div`
   flex: 1;
+  padding: 20px;
 `;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
 const Right = styled.div`
   flex: 1;
+  padding: 20px;
 `;
+
+const ContactItem = styled.div`
+margin-bottom: 20px;
+display: flex;
+align-items: center;
+`
+
+const Payment = styled.img`
+width: 50%;
+`
 
 export const Footer = () => {
   return (
@@ -67,9 +97,32 @@ export const Footer = () => {
         </SocialContainer>
       </Left>
 
-      <Center></Center>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Games</ListItem>
+          <ListItem>Consoles</ListItem>
+          <ListItem>Accessories</ListItem>
+        </List>
+      </Center>
 
-      <Right></Right>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <MapIcon style={{ marginRight: "10px" }} />3 Abbey Rd
+        </ContactItem>
+        <ContactItem>
+          <PermPhoneMsgIcon style={{ marginRight: "10px" }} />
+          +1 000-000-0000
+        </ContactItem>
+        <ContactItem>
+          <EmailIcon style={{ marginRight: "10px" }} />
+          email@email.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/580r0pC/paymentoptions.png" />
+      </Right>
     </Container>
   );
-}
+};
