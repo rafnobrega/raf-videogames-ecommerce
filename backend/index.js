26ebcb0f00200bd061e03e73e152d678b8c3d677
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-08-03 01:31:03 -0700

    fix issue with CrptoJS

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
+
+
 
 dotenv.config();
 
@@ -15,10 +18,10 @@ mongoose
   });
 
 app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
-app.use("api/users", userRoute);
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
+const PORT = process.env.API_PORT;
+app.listen(PORT || 5003, () => {
   console.log(`Backend server is running on PORT ${PORT}`);
 });
--- backend/models/User.js
+++ backend/models/User.js
@@ -2,12 +2,15 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique },
-    email: { type: String, required: true, unique },
-    password: { type: String, required },
-    isAdmin: { type: Boolean, default: false },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("User", UserSchema)
+module.exports = mongoose.model("User", UserSchema);
