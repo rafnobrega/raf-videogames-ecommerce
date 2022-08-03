b01c6615c50be937b4f576ca2dbbf68e413d9cf6
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-08-03 15:03:20 -0700

    product route

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 
 
 
@@ -20,6 +21,7 @@ mongoose
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 const PORT = process.env.API_PORT;
 app.listen(PORT || 5003, () => {
