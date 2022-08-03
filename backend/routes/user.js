6041e7ee4a82b227f583c3731a61dad065ecc9a9
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-08-03 12:45:54 -0700

    create user route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,28 @@
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+
 const router = require("express").Router();
 
+// UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      CryptoJS.enc.Utf8.parse(req.body.password),
+      process.env.PASSWORD_SECRET
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
