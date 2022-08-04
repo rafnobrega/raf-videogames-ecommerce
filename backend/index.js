68a6c4a82f435d93178cdf624e75a537ffa31f16
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-08-04 12:26:33 -0700

    fix stripe key

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,10 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+
+
+const cors = require("cors");
 
 dotenv.config();
 
@@ -17,12 +21,14 @@ mongoose
     console.log(err);
   });
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 const PORT = process.env.API_PORT;
 app.listen(PORT || 5003, () => {
--- backend/routes/stripe.js
+++ backend/routes/stripe.js
@@ -1,13 +1,14 @@
 const router = require("express").Router();
-
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
+const KEY = process.env.STRIPE_KEY;
+const stripe = require("stripe")(KEY);
 
 router.post("/payment", (req, res) => {
   stripe.charges.create(
     {
       source: req.body.tokenId,
       amount: req.body.amount,
-      currency: "cad",
+      currency: "usd",
     },
     (stripeErr, stripeRes) => {
       if (stripeErr) {
