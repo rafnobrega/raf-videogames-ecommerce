b6198d9d9b595e7c5c5e5da9099f97077ea77caf
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-08-04 12:38:23 -0700

    fix product model with inStock

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -7,6 +7,7 @@ const ProductSchema = new mongoose.Schema(
     img: { type: String, required: true },
     categories: { type: Array },
     price: { type: Number, required: true },
+    inStock: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,26 +4,9 @@ import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
 
 const App = () => {
-  // return <Home />;
-  return (
-    <Router>
-      <Routes>
-        <Route path="/pay">
-          {/* <Pay/> */}
-        </Route>
-        <Route path="/success">
-          {/* <Success/> */}
-        </Route>
-      </Routes>
-    </Router>
-  )
+  return <Home />;
 };
 
 export default App;
