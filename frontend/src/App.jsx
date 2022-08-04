54e22de457a535fbc8331c64d8f4303b53e3015d
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-08-04 12:26:59 -0700

    fix stripe keys in BE and FE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,26 @@ import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const App = () => {
-  return <Home />;
+  // return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/pay">
+          {/* <Pay/> */}
+        </Route>
+        <Route path="/success">
+          {/* <Success/> */}
+        </Route>
+      </Routes>
+    </Router>
+  )
 };
 
 export default App;
