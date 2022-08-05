0a9685d0b7635707a0995f0b833434b2670fbcc9
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-08-05 11:19:29 -0700

    add useEffect to filter products in Products component

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { popularProducts } from "../data";
-import Product from "./Product"
+import Product from "./Product";
+import axios from "axios";
 
 const Container = styled.div`
   padding: 20px;
@@ -10,14 +11,46 @@ const Container = styled.div`
   justify-content: space-between;
 `;
 
-const Products = ({category, filters, sort}) => {
-console.log(category, filters, sort);
+const Products = ({ category, filters, sort }) => {
+  // console.log(category, filters, sort);
 
-  return <Container>
-    {popularProducts.map((item) => (
-      <Product item={item}  key={item.id}/>
-    ))}
-  </Container>;
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          category
+            ? `http://localhost:5003/api/products?category=${category}`
+            : "http://localhost:5003/api/products"
+        );
+        setProducts(res.data);
+      } catch (err) {}
+    };
+    getProducts();
+  }, [category]);
+
+  useEffect(() => {
+    category &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [products, category, filters]);
+
+
+
+  return (
+    <Container>
+      {filteredProducts.map((item) => (
+        <Product item={item} key={item.id} />
+      ))}
+    </Container>
+  );
 };
 
 export default Products;
