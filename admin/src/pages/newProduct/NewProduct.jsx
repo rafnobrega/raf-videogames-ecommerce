68bf0ee3be051098953749aa9581d0742feda249
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-08-21 18:09:04 -0700

    configure useState hook for NewProduct in the admin folder

--- admin/src/pages/newProduct/NewProduct.jsx
+++ admin/src/pages/newProduct/NewProduct.jsx
@@ -1,34 +1,76 @@
+import { useState } from "react";
 import "./newProduct.css";
 
 export default function NewProduct() {
+  const [inputs, setInputs] = useState({});
+  const [file, setFile] = useState(null);
+  const [categories, setCategories] = useState([]);
+
+  const handleChange = (event) => {
+    setInputs((prev) => {
+      return { ...prev, [event.target.name]: event.target.value };
+    });
+  };
+  const handleCategories = (event) => {
+    setCategories(event.target.value.split(","));
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    
+  }
+
   return (
     <div className="newProduct">
       <h1 className="addProductTitle">New Product</h1>
       <form className="addProductForm">
         <div className="addProductItem">
           <label>Image</label>
-          <input type="file" id="file" />
+          <input
+            type="file"
+            id="file"
+            onChange={(event) => setFile(event.target.files)[0]}
+          />
         </div>
         <div className="addProductItem">
           <label>Title</label>
-          <input type="text" placeholder="" />
+          <input
+            name="title"
+            type="text"
+            placeholder=""
+            onChange={handleChange}
+          />
         </div>
         <div className="addProductItem">
           <label>Description</label>
-          <input type="text" placeholder="" />
+          <input
+            name="description"
+            type="text"
+            placeholder=""
+            onChange={handleChange}
+          />
         </div>
         <div className="addProductItem">
           <label>Price</label>
-          <input type="text" placeholder="1,000.00" />
+          <input
+            name="price"
+            type="number"
+            placeholder="1,000.00"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addProductItem">
+          <label>Categories</label>
+          <input type="text" placeholder="" onChange={handleCategories} />
         </div>
         <div className="addProductItem">
           <label>Stock</label>
-          <select>
+          <select name="inStock" onChange={handleChange}>
             <option value="true">Yes</option>
             <option value="false">No</option>
           </select>
         </div>
-        <button className="addProductButton">Create</button>
+        <button onClick={handleClick} className="addProductButton">Create</button>
       </form>
     </div>
   );
